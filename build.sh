f8e5ca2b9518487d420011b68e932a0da07a2d1a
Eden <161572512+edenqyb@users.noreply.github.com>
2025-09-21 08:35:01 +0330

    Initial Commit.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# variables
+REGISTRY=""
+BASE_PATH=""
+TAG="latest"
+
+# list of service directories
+SERVICES=(
+)
+
+for SERVICE in "${SERVICES[@]}"; do
+  IMAGE="${REGISTRY}/${BASE_PATH}/${SERVICE}:${TAG}"
+  echo ">>> Building $IMAGE from $SERVICE"
+  docker build -t "$IMAGE" "./$SERVICE"
+  echo ">>> Pushing $IMAGE"
+  docker push "$IMAGE"
+done
+
+# build
+docker build -t "$IMAGE" .
